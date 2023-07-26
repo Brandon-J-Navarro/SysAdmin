467ca280a4589d750b6d961bc25374e703237cfb
Brandon <brandon.j.navarro@outlook.com>
2023-07-26 09:05:41 -0400

    update format

--- Docker/PortainerUpdate.sh
+++ Docker/PortainerUpdate.sh
@@ -1,22 +1,21 @@
 #FROM
-    #SOURCES : 
-      #https://github.com/novaspirit/pi-hosted
-      
+    #SOURCES: https://github.com/novaspirit/pi-hosted
+
 #!/bin/bash
 
 function error {
-  echo -e "\\e[91m$1\\e[39m"
-  exit 1
+    echo -e "\\e[91m$1\\e[39m"
+    exit 1
 }
 
 function check_internet() {
-  printf "Checking if you are online..."
-  wget -q --spider http://github.com
-  if [ $? -eq 0 ]; then
-    echo "Online. Continuing."
-  else
-    error "Offline. Go connect to the internet then run the script again."
-  fi
+    printf "Checking if you are online..."
+    wget -q --spider http://github.com
+    if [ $? -eq 0 ]; then
+        echo "Online. Continuing."
+    else
+        error "Offline. Go connect to the internet then run the script again."
+    fi
 }
 
 check_internet
