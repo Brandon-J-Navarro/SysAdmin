34376631e4b044eaf59a7f65494c9b83d8c05215
Brandon <brandon.j.navarro@outlook.com>
2023-07-26 09:06:28 -0400

    update format

--- Bash/ApacheGuacamoleInstall.sh
+++ Bash/ApacheGuacamoleInstall.sh
@@ -6,16 +6,12 @@ wget https://git.io/fxZq5 -O guac-install.sh
 chmod +x guac-install.sh
 
 #Run it as root:
-Interactive (asks for passwords):
-
+# Interactive (asks for passwords):
 ./guac-install.sh
 
 #Non-Interactive (values provided via cli):
-
 ./guac-install.sh --mysqlpwd [PASSWORD] --guacpwd [PASSWORD] --nomfa --installmysql
-
 #OR
-
 ./guac-install.sh -r [PASSWORD] -gp [PASSWORD] -o -i
 
 #Once installation is done you can access Guacamole by browsing to: http://<host_or_ip>:8080/guacamole/ The default credentials are guacadmin as both username and password. Please change them or disable guacadmin after install!
